fa73239f2dc1afca360650026506059b4837f772
Solankimimoh <solankimimoh@gmail.com>
2018-07-29 13:23:07 +0530

    menu & css selector, html5 tag layout

---
+++ day9/cssselector.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Title </title>
+    <style>
+
+        a[href]
+        {
+            color: orange;
+        }
+        
+        a[href=test]
+        {
+            font-size: 40px;
+        }
+
+    </style>
+</head>
+
+<body>
+
+    <a href="test">GOTO WEBSITE</a>
+    <a href="">GOTO VIDEO</a>
+    <a>GOTO MUSIC</a>
+    <a>GOTO GALLERY</a>
+
+</body>
+
+</html>
---
+++ day9/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Title </title>
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+
+    <header>
+
+        <nav>
+            <div class="brand">
+
+            </div>
+            <div class="menuContainer">
+                <ul class="menu">
+                    <li><a href="#homeSection">HOME</a></li>
+                    <li><a href="#aboutSection">ABOUT</a></li>
+                    <li><a href="">GALLERY</a></li>
+                    <li><a href="">CONTACT US</a></li>
+                </ul>
+            </div>
+
+        </nav>
+    </header>
+
+    <div id="homeSection" class="home">
+
+    </div>
+    <div id="aboutSection" class="about">
+        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/Qnm2n_7uYf0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </div>
+
+
+
+</body>
+
+</html>
